4a7d1d6d7539be9dcf7fc548e4b4b205b15e5eda
Scion-Kin <dinturner17@gmail.com>
2024-06-06 15:14:41 +0200

    Task 0 done. Project complete.

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,35 +3,25 @@
 
 
 def canUnlockAll(boxes):
-    ''' This functions checks if all boxes can be opened or not '''
+    ''' This function checks if all boxes can be opened or not '''
 
     if not boxes:
         return False
 
     keys = set(boxes[0])
-    boxes[0] = 'opened'
+    opened_boxes = {0}
 
     while True:
-        if len(keys) == 0 and len([i for i in boxes if i != 'opened']) > 0:
+        new_keys_found = False
+
+        for key in list(keys):
+            if key < len(boxes) and key not in opened_boxes:
+                opened_boxes.add(key)
+                keys.update(boxes[key])
+                new_keys_found = True
+
+        if len(opened_boxes) == len(boxes):
+            return True
+
+        if not new_keys_found:
             return False
-
-        elif len(keys) == 0 and len([i for i in boxes if i != 'opened']) == 0:
-            return True
-
-        elif len([i for i in boxes if i == 'opened']) == len(boxes):
-            return True
-
-        else:
-            box_found = False
-            for key in keys:
-                if boxes[key] != 'opened':
-                    box_found = True
-                    keys.update([i for i in boxes[key] if i <
-                                 len(boxes) and i != 0])
-                    boxes[key] = 'opened'
-                    keys.remove(key)
-                    break
-
-            if not box_found:
-
-                return False
