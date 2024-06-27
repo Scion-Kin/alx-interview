9e05e593c7bf06496a8bb7db9dab72dabb4f7479
Scion-Kin <dinturner17@gmail.com>
2024-06-27 11:29:54 +0200

    Project complete!

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,31 +2,25 @@
 ''' This defines a function '''
 
 
-def recur(li):
-    ''' Handles execution with recursion '''
-
-    continue_indicators = {194: 2, 229: 3, 240: 4}
-
-    if len(li) == 0:
-        return True
-
-    if li[0] < 128:
-        return recur(li[1:])
-
-    if li[0] not in continue_indicators:
-        return False
-
-    if len(li) < continue_indicators[li[0]]:
-        return False
-
-    for i in li[1:continue_indicators[li[0]] + 1]:
-        if i < 128 or i > 191:
-            return False
-
-    return recur(li[continue_indicators[li[0]]:])
-
-
 def validUTF8(data):
     ''' This validates if a sequence in a list is UTF-8 compliant '''
 
-    return recur(data) if len(data) > 0 else False
+    continue_indicators = {194: 2, 229: 3, 240: 4}
+
+    if len(data) == 0:
+        return True
+
+    if data[0] < 128:
+        return validUTF8(data[1:])
+
+    if data[0] not in continue_indicators:
+        return False
+
+    if len(data) < continue_indicators[data[0]]:
+        return False
+
+    for i in data[1:continue_indicators[data[0]] + 1]:
+        if i < 128 or i > 191:
+            return False
+
+    return validUTF8(data[continue_indicators[data[0]]:])
