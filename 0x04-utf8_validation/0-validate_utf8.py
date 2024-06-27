2d78c78c25a97ef2afe8dfc7810f2e057ea5d4cc
Scion-Kin <dinturner17@gmail.com>
2024-06-27 13:10:28 +0200

    Project complete

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' This defines a function '''
 
+
 def validUTF8(data):
     ''' This validates if a sequence in a list is UTF-8 compliant '''
 
@@ -15,13 +16,20 @@ def validUTF8(data):
         if (byte & 0b10000000) == 0:  # 1-byte character (0xxxxxxx)
             i += 1
             continue
-        elif (byte & 0b11100000) == 0b11000000:  # 2-byte character (110xxxxx 10xxxxxx)
+        elif (byte & 0b11100000) == 0b11000000:
+            # 2-byte character (110xxxxx 10xxxxxx)
             num_bytes = 2
-        elif (byte & 0b11110000) == 0b11100000:  # 3-byte character (1110xxxx 10xxxxxx 10xxxxxx)
+
+        elif (byte & 0b11110000) == 0b11100000:
+            # 3-byte character (1110xxxx 10xxxxxx 10xxxxxx)
             num_bytes = 3
-        elif (byte & 0b11111000) == 0b11110000:  # 4-byte character (11110xxx 10xxxxxx 10xxxxxx 10xxxxxx)
+
+        elif (byte & 0b11111000) == 0b11110000:
+            # 4-byte character (11110xxx 10xxxxxx 10xxxxxx 10xxxxxx)
             num_bytes = 4
-        else:  # Invalid first byte
+
+        else:
+            # Invalid first byte
             return False
 
         # Check if there are enough continuation bytes
