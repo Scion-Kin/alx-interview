5c7fbe6ccf82fa3a6bb2791a63e62bc7b2bf2eb9
Scion-Kin <dinturner17@gmail.com>
2024-06-27 09:38:24 +0200

    Project complete!

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,16 +2,16 @@
 ''' This defines a function '''
 
 
-def validUTF8(data, valid=False):
+def validUTF8(data):
     ''' This validates if a sequence in a list is UTF-8 compliant '''
 
     continue_indicators = {194: 2, 229: 3, 240: 4}
 
     if len(data) == 0:
-        return True if valid == True else False
+        return True
 
     if data[0] < 128:
-        return validUTF8(data[1:], True)
+        return validUTF8(data[1:])
 
     if data[0] not in continue_indicators:
         return False
@@ -20,4 +20,4 @@ def validUTF8(data, valid=False):
         if i < 128 or i > 191:
             return False
 
-    return validUTF8(data[continue_indicators[data[0]] + 1:], True)
+    return validUTF8(data[continue_indicators[data[0]] + 1:])
