cb512b04ff348f232e183193cdcfced57e114a89
Scion-Kin <dinturner17@gmail.com>
2024-06-27 09:32:21 +0200

    Project complete!

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+''' This defines a function '''
+
+
+def validUTF8(data):
+    ''' This validates if a sequence in a list is UTF-8 compliant '''
+
+    continue_indicators = {194: 2, 229: 3, 240: 4}
+
+    if len(data) == 0:
+        return True
+
+    if data[0] < 128:
+        return validUTF8(data[1:])
+
+    if data[0] not in continue_indicators:
+        return False
+
+    for i in data[1:continue_indicators[data[0]] + 1]:
+        if i < 128 or i > 191:
+            return False
+
+    return validUTF8(data[continue_indicators[data[0]] + 1:])
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,1 @@
+This directory contains the solution to the UTF-8 validation project
