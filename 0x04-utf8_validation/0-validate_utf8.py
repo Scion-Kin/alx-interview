5d6e04fda7b14ca09a407f1988c9d7665ddaa9ac
Scion-Kin <dinturner17@gmail.com>
2024-06-27 10:02:37 +0200

    Trying to cover the unknown edge cases

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -13,11 +13,13 @@ def validUTF8(data):
     if data[0] < 128:
         return validUTF8(data[1:])
 
-    if data[0] not in continue_indicators or\
-            len(data) < continue_indicators[data[0]]:
+    if data[0] not in continue_indicators:
         return False
 
-    for i in data[1:continue_indicators[data[0]] + 1]:
+    if len(data) < continue_indicators[data[0]]:
+        return False
+
+    for i in data[1:continue_indicators[data[0]]]:
         if i < 128 or i > 191:
             return False
 
