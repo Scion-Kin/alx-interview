af9f22defa095b932d39b7cc2674fc7a4a4772b7
Scion-Kin <dinturner17@gmail.com>
2024-06-27 09:48:39 +0200

    Project complete!

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -13,11 +13,13 @@ def validUTF8(data):
     if data[0] < 128:
         return validUTF8(data[1:])
 
-    if data[0] not in continue_indicators:
+    if data[0] > 255:
         return False
 
-    for i in data[1:continue_indicators[data[0]] + 1]:
-        if i < 128 or i > 191:
-            return False
+    if data[0] in continue_indicators:
+        for i in data[1:continue_indicators[data[0]]]:
+            if i < 128 or i > 191:
+                return False
 
-    return validUTF8(data[continue_indicators[data[0]] + 1:])
+    return validUTF8(data[continue_indicators[data[0]]:]) if\
+        data[0] in continue_indicators else data[1:]
