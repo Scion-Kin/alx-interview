73946dc08e98b15c53fa0c2617f9cab7a5e99b57
Scion-Kin <dinturner17@gmail.com>
2024-07-16 19:25:30 +0200

    Project complete

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,13 +6,13 @@ def rotate_2d_matrix(matrix):
     ''' This prints a 90 degree rotated version of the given matrix '''
 
     for i in range(0, len(matrix)):
-        ''''''
+
         to = (len(matrix[i]) - 1 - i)
         if i == len(matrix) - 1:
-            to = len(matrix) - 1
+            to = len(matrix[i]) - 1
 
         for j in range(to, -1, -1):
-            ''''''
+
             indice = len(matrix) - 1 - i
             temp = matrix[j][indice]
             matrix[j][indice] = matrix[i][j]
