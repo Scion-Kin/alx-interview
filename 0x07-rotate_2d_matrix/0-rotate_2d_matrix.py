d8bc3a1f689106a4ad4af8da21b6f23eb64381d9
Scion-Kin <dinturner17@gmail.com>
2024-07-17 17:05:51 +0200

    Project complete

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
 ''' This defines a function '''
 
-from time import sleep
-
 
 def rotate_2d_matrix(matrix):
-    ''' This prints a 90 degree rotated version of the given matrix '''
+    ''' This rotates a matrix to 90 degrees'''
 
     grand = [[] for i in matrix]
 
-    for j in range(len(matrix)):
-        for i in range(len(matrix) - 1, -1, -1):
-            grand[j].append(matrix[i][j])
+    for i in range(len(matrix)):
+        for j in range(len(matrix) - 1, -1, -1):
+            grand[i].append(matrix[j][i])
 
     for i in range(len(matrix)):
         matrix[i] = grand[i]
+
+
+'''
+    The solution to this problem is not literally swapping the values in
+    the matrix. Rather, it is copying the indices in a new list,
+    in a way that they will be rotated.
+'''
