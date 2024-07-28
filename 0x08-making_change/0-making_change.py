9e80f47c8a86596c00311a0457f4f0b24938da1a
Scion-Kin <dinturner17@gmail.com>
2024-07-28 13:01:51 +0200

    Project complete

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,11 +3,11 @@
 
 
 def makeChange(coins, total):
-    ''' This returns the lowest amount of coin flips
+    ''' This returns the lowest total of coin flips
         needed to make change for a given total '''
 
     # This table will store the answer to our sub problems
-    dp = [amount + 1] * (amount + 1)
+    dp = [total + 1] * (total + 1)
 
     '''
     The answer to making change with minimum coins for 0
@@ -16,21 +16,21 @@ def makeChange(coins, total):
     '''
     dp[0] = 0
 
-    # Solve every subproblem from 1 to amount
-    for i in range(1, amount + 1):
+    # Solve every subproblem from 1 to total
+    for i in range(1, total + 1):
         # For each coin we are given
         for j in range(0, len(coins)):
-            # If it is less than or equal to the sub problem amount
+            # If it is less than or equal to the sub problem total
             if coins[j] <= i:
                 # Try it. See if it gives us a more optimal solution
                 dp[i] = min(dp[i], dp[i - coins[j]] + 1)
 
     '''
-    dp[amount] has our answer. If we do not have an answer then dp[amount]
-    will be amount + 1 and hence dp[amount] > amount will be true. We then
+    dp[total] has our answer. If we do not have an answer then dp[total]
+    will be total + 1 and hence dp[total] > total will be true. We then
     return -1.
 
-    Otherwise, dp[amount] holds the answer
+    Otherwise, dp[total] holds the answer
     '''
 
-    return -1 if dp[amount] > amount else dp[amount]
+    return -1 if dp[total] > total else dp[total]
