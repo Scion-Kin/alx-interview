82ff333bc3c70b9ce2cb8ae118bcc8a06aeb114f
Scion-Kin <dinturner17@gmail.com>
2024-07-28 13:00:24 +0200

    Project complete

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,33 +6,31 @@ def makeChange(coins, total):
     ''' This returns the lowest amount of coin flips
         needed to make change for a given total '''
 
-    coins = sorted(coins)
-    coins.reverse()
+    # This table will store the answer to our sub problems
+    dp = [amount + 1] * (amount + 1)
 
-    def recur(coins, coin, total, count=0):
-        ''' This solves the problem recursively '''
+    '''
+    The answer to making change with minimum coins for 0
+    will always be 0 coins no matter what the coins we are
+    given are
+    '''
+    dp[0] = 0
 
-        if total == 0:
-            return count
+    # Solve every subproblem from 1 to amount
+    for i in range(1, amount + 1):
+        # For each coin we are given
+        for j in range(0, len(coins)):
+            # If it is less than or equal to the sub problem amount
+            if coins[j] <= i:
+                # Try it. See if it gives us a more optimal solution
+                dp[i] = min(dp[i], dp[i - coins[j]] + 1)
 
-        if total < 0 or total - coin < 0:
-            nextcoin = coins.index(coin) + 1
-            if nextcoin == len(coins):
-                return -1
+    '''
+    dp[amount] has our answer. If we do not have an answer then dp[amount]
+    will be amount + 1 and hence dp[amount] > amount will be true. We then
+    return -1.
 
-            return recur(coins, coins[nextcoin], total - coins[nextcoin],
-                         count + 1)
+    Otherwise, dp[amount] holds the answer
+    '''
 
-        return recur(coins, coin, total - coin, count + 1)
-
-    all = []
-    for i in coins:
-        try:
-            all.append(recur(coins, i, total))
-
-        except RecursionError:
-            pass
-
-    all = [i for i in all if i != -1]
-
-    return 0 if total <= 0 else min(all) if len(all) != 0 else -1
+    return -1 if dp[amount] > amount else dp[amount]
