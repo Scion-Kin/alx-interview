7b5caf2ac7f8c65da3638bac50b01db2787ba067
Scion-Kin <dinturner17@gmail.com>
2024-07-28 12:21:43 +0200

    Project complete

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+''' This defines a function '''
+
+
+def makeChange(coins, total):
+    ''' This returns the lowest amount of coin flips
+        needed to make change for a given total '''
+
+    coins = sorted(coins)
+    coins.reverse()
+
+    def recur(coins, coin, total, count=0):
+        ''' This solves the problem recursively '''
+
+        if total == 0:
+            return count
+
+        if total < 0 or total - coin < 0:
+            nextcoin = coins.index(coin) + 1
+            if nextcoin == len(coins):
+                return -1
+
+            return recur(coins, coins[nextcoin], total - coins[nextcoin],
+                         count + 1)
+
+        return recur(coins, coin, total - coin, count + 1)
+
+    all = []
+    for i in coins:
+        all.append(recur(coins, i, total))
+
+    all = [i for i in all if i != -1]
+
+    return 0 if total <= 0 else min(all) if len(all) != 0 else -1
